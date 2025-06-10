5e811d53eba4718f197d140da598281af208e0f3
unknown <joaomaltar14@gmail.com>
2025-06-09 22:26:40 -0300

    redirect ao clicar no card e registro de contagem

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@ from flask import Flask,  render_template, request, redirect, jsonify
 
 app = Flask(__name__)
 
-conjuntos_teste = [["Contagem 1", "valor1", "valor2"], ["Contagem 2", "valor1", "valor2"]]
+conjuntos_teste = []
 
 @app.route("/")
 def home():
@@ -21,22 +21,19 @@ def registra_conjunto():
 
 
 
-# @app.route("/conjuntos/")
-# def conjuntos():
-#     return render_template('html/conjuntos.html')
+@app.route("/conjuntos/<string:titulo_conjunto>")
+def contadores(titulo_conjunto):
+    for i in conjuntos_teste:
+        if(i[0] == titulo_conjunto):
+            print(f"achou o conjunto {i[0]}")
+            return render_template('html/conjuntos.html', contadores_conjunto = i[1:])
+
 
 # @app.route("/conjuntos/detalhes")
 # def detalhes():
 #     print(f"conjuntos {conjuntos_teste}")
 #     return render_template('html/detalhes.html', conjuntos=conjuntos_teste)
 
-# @app.route("/registra", methods = ['POST'])
-# def registra():
-#     if request.method == 'POST':
-#         nova_contagem  = request.form.get('titulo')
-#         conjuntos_teste.append([nova_contagem,'aa','bb'])
-#         print("salvou")
-#         return redirect("/conjuntos/detalhes")
 
 if __name__ == "__main__":
     app.run()
