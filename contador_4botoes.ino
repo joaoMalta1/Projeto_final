54c6e8d51b47401d072ffa351d4e6547c33ccd29
bellagomesp <115584209+bellagomesp@users.noreply.github.com>
2025-06-18 19:00:19 -0300

    atualizacao para struct e eeprom

--- contador_4botoes.ino
+++ contador_4botoes.ino
@@ -5,21 +5,32 @@
 
 
 MCUFRIEND_kbv tela;
-int qtd[4] = {0, 99, 999, 1000};
 unsigned long temposPressionados[4] = {0, 0, 0, 0};
 int space[4] = {0, 0, 0, 0};
-String unidades[4] = {"g", "M", "L", "mm"};
-String contagem = "Setor";
+String nome_contagem = "";
+byte num = 0;
+
+struct contagem{
+  String nome ;
+  String unidade;
+  int passo;
+  int qtd;
+}contagem[4]; 
+
 
 int altura = 50;
 bool posicaoAnterior; 
 bool posicao;
 bool contagemAlterada = false;
 unsigned long ultimaAlteracao = 0;
+byte botao_press[100];
 
-int enderecoQtd = 0; //8 bytes -> de 0 a 7
-int enderecoContagem = enderecoQtd + 8; // 1 + 20 (max de caracteres) ->8 a 28
-int enderecoUnidades = enderecoContagem + 21; //4 unidades de ate 5 letras = 24 bytes -> 29 a 52
+
+int tam_contagem = sizeof(contagem);
+int tam_total = 0;
+int tam_historico_botoes = 0;
+int tam_botoes_historico_total = 0;
+
 
 GFButton btn1(A8);
 GFButton btn2(A9);
@@ -30,7 +41,7 @@ void setup() {
 
   Serial.begin(9600);
   carregarDadosDaEEPROM();
-  // esperarDadosSerial();
+  esperarDadosSerial();
 
   tela.begin(tela.readID());
   tela.fillScreen(TFT_BLACK);
@@ -59,12 +70,14 @@ void loop () {
 	btn3.process();
 	btn4.process();
 
-  // esperarDadosSerial();
+  esperarDadosSerial();
 
   if (contagemAlterada && (millis() - ultimaAlteracao > 5000)) {
     salvarDadosNaEEPROM();
     contagemAlterada = false;
+    
     Serial.println("Dados salvos após 5 segundos");
+    tam_historico_botoes = 0;
   }
 
 }
@@ -82,11 +95,9 @@ void verificaPressionado(GFButton& botao) {
   else if (&botao == &btn4) 
 	  i = 3;
 
-
   temposPressionados[i] = millis();
 
 }
-
 
 void aumentaOuDiminui(GFButton& botao){
   int indice;
@@ -101,28 +112,34 @@ void aumentaOuDiminui(GFButton& botao){
 
 
   if ( millis() - temposPressionados[indice] > 1000) {
-    if (qtd[indice] > 0) {
+
+    if (contagem[indice].qtd > 0) {
       diminuirContagem(botao, indice);
     } else {
-      qtd[indice] = 0;
+      contagem[indice].qtd = 0;
     }
+    botao_press[tam_historico_botoes] = -(indice + 1);
+    
+    Serial.println(botao_press[tam_historico_botoes]);
+    tam_historico_botoes++;
   }
   else if (millis() - temposPressionados[indice] < 1000){
     aumentarContagem(botao, indice);
+    botao_press[tam_historico_botoes] = (indice + 1);
+    Serial.println(botao_press[tam_historico_botoes]);
+    tam_historico_botoes++;
   }
 
 
 }
-//DUVIDA: será que colocar para salvar a cada vez que diminuir ou amentar a contagem nao irá deixar lento o processo?
-//Talvez possa ter um timer para ver quando a pessoa parou de mexer ou colocar um quinto botao para quando a pessoa pressionar os dados sejam salvos
 void aumentarContagem(GFButton& botao, int indice) {
-  qtd[indice]++;
+  contagem[indice].qtd++;
   atualizaTela(indice);
   contagemAlterada = true;
   ultimaAlteracao = millis();
 }
 void diminuirContagem(GFButton& botao, int indice) {
-  qtd[indice]--;
+  contagem[indice].qtd--;
   atualizaTela(indice);
   contagemAlterada = true;
   ultimaAlteracao = millis();
@@ -132,9 +149,9 @@ void mostrarPrimeiraColuna(){
 	tela.setTextColor(TFT_WHITE);
   tela.setTextSize(3);
   tela.setCursor(15, 30);
-  tela.print(contagem);
+  tela.print(nome_contagem);
 
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < num; i++) {
     int y = 80 + i * altura ;
 
     tela.setCursor(15, y + i*5 + 15);
@@ -142,7 +159,7 @@ void mostrarPrimeiraColuna(){
     escolheCor(i);
 
     tela.setTextSize(2);
-    tela.print(contagem + " " + String(i + 1));
+    tela.print(contagem[i].nome);
   }
 }
 
@@ -153,54 +170,54 @@ void mostrarSegundaColuna() {
   tela.print("QTD");
   
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < num; i++) {
     int y = 90 + i * altura;
-    if(qtd[i] < 10){
+    if( contagem[i].qtd < 10){
       space[i] = 55;
     }
-    else if(qtd[i] >= 10 && qtd[i] < 100){
+    else if( contagem[i].qtd >= 10 && contagem[i].qtd < 100){
       space[i] = 38;
     }
-    else if(qtd[i] >= 100 && qtd[i] < 1000){
+    else if(contagem[i].qtd >= 100 && contagem[i].qtd < 1000){
       space[i] = 19;
     }
-    else if(qtd[i] >= 1000 && qtd[i] < 9999){
+    else if(contagem[i].qtd >= 1000 && contagem[i].qtd < 9999){
       space[i] = 1;
     }
     
-    Serial.println(String(unidades[i].length()));
-    space[i] -= (unidades[i].length()*18); //largura da letra é 6 pixels no tamanho 3 = 18
+    Serial.println(String(contagem[i].unidade.length()));
+    space[i] -= (contagem[i].unidade.length()*18); //largura da letra é 6 pixels no tamanho 3 = 18
     tela.setCursor(150 + space[i], y + i *6);  
     
     escolheCor(i);
 
     tela.setTextSize(3);
-    tela.print(String(qtd[i])+unidades[i]);
+    tela.print(String(contagem[i].qtd)+contagem[i].unidade);
     
   }
 }
 
 void atualizaTela(int i){
   int y = 90 + i * altura;
-  if(qtd[i] < 10){
+  if(contagem[i].qtd < 10){
     space[i] = 55;
   }
-  else if(qtd[i] >= 10 && qtd[i] < 100){
+  else if(contagem[i].qtd >= 10 && contagem[i].qtd < 100){
     space[i] = 38;
   }
-  else if(qtd[i] >= 100 && qtd[i] < 1000){
+  else if(contagem[i].qtd >= 100 && contagem[i].qtd < 1000){
     space[i] = 19;
   }
-  else if(qtd[i] >= 1000 && qtd[i] < 9999){
+  else if(contagem[i].qtd >= 1000 && contagem[i].qtd < 9999){
     space[i] = 1;
   }
   
   tela.fillRect(110, y+ i *6, 120, 35, TFT_BLACK);
-  space[i] -= (unidades[i].length()*18);
+  space[i] -= (contagem[i].unidade.length()*18);
   tela.setCursor(150 + space[i], y + i *6);  
   
   escolheCor(i);
-  tela.print(String(qtd[i])+unidades[i]);
+  tela.print(String(contagem[i].qtd)+contagem[i].unidade);
 
   tela.setTextSize(3);
   
@@ -223,113 +240,124 @@ void escolheCor(int i){
 
 }
 
-void salvarDadosNaEEPROM() {
-  // Salvar qtd[4]
-  for (int i = 0; i < 4; i++) {
-    EEPROM.put(enderecoQtd + i * sizeof(int), qtd[i]);
-  }
-
-  // Salvar contagem 
-  byte tamanhoContagem = contagem.length(); //cada letra ocupa um byte
-  EEPROM.put(enderecoContagem, tamanhoContagem);
-  for (int i = 0; i < tamanhoContagem; i++) {
-    EEPROM.put(enderecoContagem + 1 + i, contagem[i]);
+void salvarDadosNaEEPROM() { 
+  //preciso salvar o titulo
+  EEPROM.put(0, num);
+  // Salvar num structs
+  for (int i = 0; i < num; i++){
+    EEPROM.put((tam_contagem * i) + 1, contagem[i]);
   }
 
 
-  // Salvar unidades[4] 
-  int enderecoAtual = enderecoUnidades;
-  for (int i = 0; i < 4; i++) {
-    byte tam = unidades[i].length(); //tamanho da stirng de unidade 
-    EEPROM.put(enderecoAtual++, tam); //salva o tamanho e move para o proximo endereco
-    for (int j = 0; j < tam; j++) { //salva letra por letra
-      EEPROM.put(enderecoAtual++, unidades[i][j]);
-    }
+  //salvar ordem de botoes pressionados 
+  tam_total = (tam_contagem * num) + 1;
+  EEPROM.put(tam_total + 1, tam_historico_botoes ); //por ser int ocupa 2 bytes
+  
+  for (int i = 0; i < tam_historico_botoes; i++ ){
+    EEPROM.put(tam_total + 2 + i + tam_botoes_historico_total , botao_press[i]);
+    
   }
+  tam_botoes_historico_total += tam_historico_botoes;  
+  
 }
+
 
 void carregarDadosDaEEPROM() {
-  // Carregar qtd[4]
-  for (int i = 0; i < 4; i++) {
-    EEPROM.get(enderecoQtd + i * sizeof(int), qtd[i]);
+  EEPROM.get(0, num);
+  // Salvar num structs
+  for (int i = 0; i < num; i++){
+    EEPROM.get((tam_contagem * i) + 1, contagem[i]);
   }
 
-  byte tamanhoContagem; //cada letra ocupa um byte
-  EEPROM.get(enderecoContagem, tamanhoContagem); //pega tamanho da string
-  contagem = ""; //garante que a string contagem esta vazia 
-  for (int i = 0; i < tamanhoContagem; i++) {
-    char c;
-    EEPROM.get(enderecoContagem + 1 + i, c);
-    contagem += c;
-  }
 
-  // Carregar unidades[4]
-  int enderecoAtual = enderecoUnidades;
-  for (int i = 0; i < 4; i++) {
-    byte tam; //tamanho da stirng de unidade 
-    EEPROM.get(enderecoAtual++, tam); //pega o tamanho da string
-    unidades[i] = ""; //garante que a string esta vazia 
-    for (int j = 0; j < tam; j++) {
-      char c;
-      EEPROM.get(enderecoAtual++, c);
-      unidades[i] += c;
-    }
+  //salvar ordem de botoes pressionados 
+  tam_total = (tam_contagem * num) + 1;
+  EEPROM.get(tam_total + 1, tam_botoes_historico_total ); //por ser int ocupa 2 bytes
+  
+  for (int i = 0; i < tam_historico_botoes; i++ ){
+    EEPROM.get(tam_total + 2 + i + tam_botoes_historico_total , botao_press[i]);
+    
   }
+  tam_botoes_historico_total += tam_historico_botoes;  
+  EEPROM.put(tam_total + 1, tam_botoes_historico_total );
+  
 }
 
-void esperarDadosSerial() {
-/*
-QTD:10,20,30,40
-Exemplo: NOME:SetorA
-Exemplo: UNI:g,L,mm,kg
 
-*/
+void esperarDadosSerial() {
+  int pos_i;
+  int pos_f;
 
   if (Serial.available() > 0) {
     String texto = Serial.readStringUntil('\n');
     texto.trim();
-
-    if (texto.startsWith("QTD:")) {
-      texto.remove(0, 4); // remove "QTD:"
-      int i = 0;
-      while (texto.length() > 0 && i < 4) {
-        int pos_virgula = texto.indexOf(',');
-        if (pos_virgula == -1){ //se não tiver virgula o resto da string sera o tamanho 
-          pos_virgula = texto.length();
-        } 
-        qtd[i] = texto.substring(0, pos_virgula).toInt();
-        texto.remove(0, pos_virgula + 1); //remove a parte usada e a virgula
-        i++;
-      }
-      Serial.println("QTD recebida!");
+     
+     //tenho que remover o [{ }] e depois de pegar o titulo apagar o contagens junto com o [{}]
+    
+    if (texto.startsWith("\"titulo\": ")){
+      pos_i = texto.indexOf(" ");
+      pos_f = texto.indexOf(",");
+      nome_contagem = texto.substring(pos_i+2, pos_f-1); // "a", -> a
+      texto.remove(0, pos_f+2); //remove até a virgula para no proximo pegar a virgula certa
+      Serial.println(nome_contagem);
+      Serial.println(texto);
+      
     }
 
-    else if (texto.startsWith("NOME:")) {
-      texto.remove(0, 5); // remove "NOME:"
-      contagem = texto;
-      Serial.println("Nome recebido!");
+    if (texto.startsWith("\"num\": ")){
+      pos_i = texto.indexOf(" ");
+      pos_f = texto.indexOf(",");
+      num = texto.substring(pos_i+2, pos_f-1).toInt();
+      Serial.println(String(num));
+      texto.remove(0, pos_f+ 2);
+      Serial.println(texto);
     }
 
-    else if (texto.startsWith("UNI:")) {
-    texto.remove(0, 4); // remove "UNI:"
     int i = 0;
-    while (texto.length() > 0 && i < 4) {
-      int pos_virgula = texto.indexOf(',');
-      if (pos_virgula == -1) { //se não tiver virgula o resto da string sera o tamanho -> ultimo valor digiado 
-        pos_virgula = texto.length();
+    while(i < num){
+      if (texto.startsWith("\"nome\": ")){
+        int pos_i = texto.indexOf(" ");
+        int pos_f = texto.indexOf(",");
+        contagem[i].nome = texto.substring(pos_i+2, pos_f-1);
+        texto.remove(0, pos_f+2);
+        Serial.println(contagem[i].nome);
       }
-      unidades[i] = texto.substring(0, pos_virgula); //pega string ate a posicao da virgula
-      texto.remove(0, pos_virgula + 1); //remove a string usada e a virgula 
-      i++;
+      if (texto.startsWith("\"passo\": ")){
+        int pos_i = texto.indexOf(" ");
+        int pos_f = texto.indexOf(",");
+        contagem[i].passo = texto.substring(pos_i+2, pos_f-1).toInt();
+        texto.remove(0, pos_f+2);
+        Serial.println(String(contagem[i].passo));
+      
+      }
+
+      if (texto.startsWith("\"unidade\": ")){
+        int pos_i = texto.indexOf(" ");
+        int pos_f = texto.indexOf(",");
+        contagem[i].unidade = texto.substring(pos_i+2, pos_f-1);
+        texto.remove(0, pos_f+2);
+        Serial.println(contagem[i].unidade);
+      }
+
+      if (texto.startsWith("\"quantidade\": ")){
+        int pos_i = texto.indexOf(" ");
+        int pos_f = texto.indexOf("\n"); //nao tenho certeza
+        contagem[i].qtd = texto.substring(pos_i+2, pos_f-1).toInt();
+        texto.remove(0, pos_f+2);
+        Serial.println(String(contagem[i].qtd));
+      }
+      atualizaTela(i); 
+
+      i++;    
     }
-    Serial.println("Unidades recebidas!");
+    mostrarPrimeiraColuna();
+
+  contagemAlterada = true;
+  ultimaAlteracao = millis();
+  
   }
 
-  dadosAlterados = true;
-  ultimaAlteracao = millis();
-
 }
-
 
 
 
